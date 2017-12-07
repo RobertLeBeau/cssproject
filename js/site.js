28775ee218ed69d8874da495e4f6a1d4a9f5bfdb
Your Name <you@example.com>
2017-12-06 23:06:27 -0600

    Increased fade in/out time on slide show, added comments

--- js/site.js
+++ js/site.js
@@ -1,17 +1,19 @@
 
-
 $.noConflict();
+
+// Re-introduce $ scope to self executing function
 (function($) {
 
   $(document).ready(function(){
 
+    // Slide show function on main page
     $(function(){
       $('.slideshow img:gt(0)').hide();
       setInterval(function(){
-        $('.slideshow :first-child').fadeOut()
-          .next('img').fadeIn()
+        $('.slideshow :first-child').fadeOut(3000)
+          .next('img').fadeIn(3000)
           .end().appendTo('.slideshow');},
-      5000);
+      5500);
     });
 
   });
